352482e8c0a76ac2af858ac521d4f968da2a39c4
华鑫 <huaxin@huaxindeMacBook-Pro.local>
2017-11-09 22:15:13 +0800

    update

--- leetcode/src/main/java/com/huaxin/medium/AddTwoNumbers.java
+++ leetcode/src/main/java/com/huaxin/medium/AddTwoNumbers.java
@@ -29,43 +29,92 @@ import com.huaxin.utils.ListNode;
  */
 public class AddTwoNumbers {
 
+    /**
+     * 时间复杂度较小
+     * @param l1
+     * @param l2
+     * @return
+     */
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode head,list = new ListNode(-1);
-        head=list;
-        int num = 0;
+        ListNode temp = l1;
+        ListNode l1_pre = l1;
+        int val;
         while (l1 != null && l2 != null) {
-            list.next=new ListNode(-1);
-            list = list.next;
-            list.val = (l1.val + l2.val) % 10 + num;
-            if ((l1.val + l2.val) >= 10) {
-                num = 1;
-                if(l1.next==null&&l2.next!=null)
-                    l2.next.val+=num;
-                else if(l2.next==null&&l1.next!=null)
-                    l1.next.val+=num;
-                else if(l1.next==null&&l2.next==null){
-                    list.next=new ListNode(num);
-                    list=list.next;
+           l1.val+=l2.val;
+           l1_pre = l1;
+           l1 = l1.next;
+           l2 = l2.next;
+        }
+        if (l1 == null) {
+            l1_pre.next = l2;
+        }
+        l1 = temp;
+        while (l1!=null) {
+            val = l1.val;
+            if (val>=10) {
+                l1.val = val%10;
+                if (l1.next==null) {
+                    l1.next = new ListNode(0);
                 }
+                l1.next.val+=val / 10;
             }
+            l1 = l1.next;
+        }
+        return temp;
+    }
+
+    /**
+     * 时间复杂度较大
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public static ListNode addTwoNumbers1(ListNode l1, ListNode l2) {
+        ListNode temp = new ListNode(0);
+        ListNode l1_temp = temp;
+        ListNode temp_pre = temp;
+        int val;
+        while (l1 != null && l2 != null) {
+            val = l1.val+l2.val;
+            temp.val += val;
+            temp.next = new ListNode(0);
+            temp_pre = temp;
+            temp = temp.next;
             l1 = l1.next;
             l2 = l2.next;
         }
-
-        return head.next;
+        if (l1 == null) {
+            temp_pre.next = l2;
+        }
+        if (l2 == null) {
+            temp_pre.next = l1;
+        }
+        temp = l1_temp;
+        while (temp!=null) {
+            val = temp.val;
+            if (val>=10) {
+                temp.val = val%10;
+                if (temp.next==null) {
+                    temp.next = new ListNode(0);
+                }
+                temp.next.val+=val / 10;
+            }
+            temp = temp.next;
+        }
+        return l1_temp;
     }
 
     public static void main(String[] args) {
-        ListNode l1=new ListNode(4);
-        l1.next=new ListNode(5);
-        l1.next.next=new ListNode(7);
+        ListNode l1=new ListNode(2);
+        l1.next=new ListNode(4);
+        l1.next.next=new ListNode(3);
         l1.next.next.next=null;
-        ListNode l2=new ListNode(3);
-        l2.next=new ListNode(5);
-        l2.next.next=new ListNode(1);
+        ListNode l2=new ListNode(5);
+        l2.next=new ListNode(6);
+        l2.next.next=new ListNode(4);
         l2.next.next.next=null;
 
-        System.out.println(AddTwoNumbers.addTwoNumbers(l1, l2).toString());
+        System.out.println(addTwoNumbers(l1, l2).toString());
     }
 
 }
