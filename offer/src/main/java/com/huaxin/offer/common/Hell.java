7b3351bc9dff12042f681f12e7c06fc91079eaf2
StevenWash <huaxin19950624@163.com>
2018-02-04 13:38:12 +0800

    update some files

---
+++ offer/src/main/java/com/huaxin/offer/common/Hell.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2018.
+ *
+ * Create by StevenWash (huaxin)
+ */
+
+package com.huaxin.offer.common;
+
+/**
+ * Description:
+ * hgffhd
+ *
+ * @author huaxin
+ * @create 2018/02/04 13:28
+ */
+public class Hell {
+}
